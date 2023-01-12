d75e728ec1aa22d1bab09499cbc30e28f31a6e12
SISUBEN <100661067+SISUBEN@users.noreply.github.com>
2023-01-12 20:36:50 +0800

    alpha test 1

---
+++ extra.js
@@ -0,0 +1,8 @@
+$(() => {
+    //@ coding: SISUBENY
+    //@ jquery: 3.6.0
+    displayThemeSeletor = function () {
+        $('#btn_group,#desc').css('display', 'none')
+        $('#theme').css('display', 'block')
+    }
+});
